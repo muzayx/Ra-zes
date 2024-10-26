a7be18fa2fb787894e34d7d063dff927aedab816
Gabriel Muzaranho <145922527+muzayx@users.noreply.github.com>
2024-10-26 15:09:32 -0300

    Add files via upload

---
+++ calcular/calcular/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calcular
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text != "")
+            {
+                double x = double.Parse(textBox1.Text);
+                x = x * x;
+                label1.Text = x.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Digite um valor na caixa de texto");
+            }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsNumber(e.KeyChar) && e.KeyChar!=',' && e.KeyChar!=(char)8)
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
